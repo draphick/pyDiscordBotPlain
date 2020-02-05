c68ea54cb98de225040c96b62d9ffd61825ccc1f
Raph Gallardo <draphick@gmail.com>
2020-02-04 18:37:03 -0800

    inital commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+# A Plain Discord Bot
+This repo contains a python file that allows you to run a discord bot. Currently just being used for training purpose.  Please ignore. :D
+
+# Base Install
+Without giving spelling out the small steps, do this:
+
++ Install Python
++ Install pip
++ Install pip requirements
++ Create a discord bot - https://discordapp.com/developers/applications/
++ Get token from discord bot you created
+  + _They call this a CLIENT SECRET in your developer's application portal_
++ Change `TOKEN` variable in `pyDiscordBot.py` to the new one you got
++ Execute the discord bot script: `python pyDiscordBot.py`
++ Send messages to your new discord bot!
+
+# Docker Setup
+Again, without all the inbetween steps written out, do the following:
+
++ Install docker
++ Use `docker build` to create a docker image using the `Dockerfile` in this repo.
++ Run a docker container mounting this repo's directory to `/discordbot/`
+  + Tip: `docker run -d -v {PATH_TO}/pyDiscordBotPlain:/discordbot:ro {dockerimagename}
++ Enjoy your containerized Discord Bot
+
+# Extra Credit
+No one likes a teacher's pet.
+
++ Add new commands to the discord bot
++ Join you discord bot to a server
++ Have someone test and break one of your bot commands
---
+++ pyDiscordBot.py
@@ -0,0 +1,71 @@
+# Work with Python 3.6
+import discord
+import urllib3
+import random
+import os
+
+TOKEN = '__GET_AND_CREATE_A_DISCORD_BOT_TOKEN__'
+
+urllib3.disable_warnings()
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # Just a catch so that it doesn't reply to itself in an infite loop
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == "bothelp":
+        await client.send_message(message.channel, """Here are all the commands I have:
+        rghelp - Provides this help tool tip.
+        dickbutt - Here's a dickbutt picture.
+        rgrps {rock|paper|scissors} - Play rock paper scissorsself. eg `rgrps rock`""")
+
+    if message.content.lower().startswith('dickbutt'):
+        embed = discord.Embed(description=message.author.mention + " wants you to look at this dick butt.", color=0xFFC0CB)
+        embed.set_image(url="https://images-na.ssl-images-amazon.com/images/I/41q1QAln%2BQL.jpg")
+        await client.send_message(message.channel, embed=embed)
+
+    if message.content.lower().startswith('botrps'):
+        rps = [
+            "rock",
+            "paper",
+            "scissors"
+        ]
+        splitmsg = message.content.split()
+        cpu = random.choice(rps)
+        if len(splitmsg) < 2:
+            await client.send_message(message.channel, "To play you need to select rock|paper|scissors.  eg. `rgrps rock`")
+        elif splitmsg[1] == "rock":
+            if cpu == "rock":
+                await client.send_message(message.channel, "YOU TIED!" + "\nComputer picked: " + cpu)
+            if cpu == "paper":
+                await client.send_message(message.channel, "YOU LOSE!" + "\nComputer picked: " + cpu)
+            if cpu == "scissors":
+                await client.send_message(message.channel, "YOU WIN!" + "\nComputer picked: " + cpu)
+        elif splitmsg[1] == "paper":
+            if cpu == "paper":
+                await client.send_message(message.channel, "YOU TIED!" + "\nComputer picked: " + cpu)
+            if cpu == "scissors":
+                await client.send_message(message.channel, "YOU LOSE!" + "\nComputer picked: " + cpu)
+            if cpu == "rock":
+                await client.send_message(message.channel, "YOU WIN!" + "\nComputer picked: " + cpu)
+        elif splitmsg[1] == "scissors":
+            if cpu == "scissors":
+                await client.send_message(message.channel, "YOU TIED!" + "\nComputer picked: " + cpu)
+            if cpu == "rock":
+                await client.send_message(message.channel, "YOU LOSE!" + "\nComputer picked: " + cpu)
+            if cpu == "paper":
+                await client.send_message(message.channel, "YOU WIN!" + "\nComputer picked: " + cpu)
+        else:
+            await client.send_message(message.channel, "You didn't pick rock paper or scriccors. Try again.")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('Bot set to: ' + TOKEN)
+    print('------')
+
+client.run(TOKEN)
